02fbd931994cbde6d1ac19faa4c0372bec08561d
Blake Hutchinson <blake.hutchinson.1996@gmail.com>
2024-05-28 03:15:58 -0500

    Added files to new repository

---
+++ src/script.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+  openMobileMenu = document.body.querySelector('#open-mobile-menu');
+  closeMobileMenu = document.body.querySelector('#close-mobile-menu');
+  mobileMenu = document.body.querySelector('#mobile-menu');
+
+  function open() {
+    openMobileMenu?.classList.add('hidden');
+    mobileMenu?.classList.add('flex', 'p-6');
+    mobileMenu?.classList.remove('hidden');
+    closeMobileMenu?.classList.remove('hidden');
+  }
+  function close() {
+    closeMobileMenu?.classList.add('hidden');
+    mobileMenu?.classList.add('hidden');
+    mobileMenu?.classList.remove('flex', 'p-6');
+    openMobileMenu?.classList.remove('hidden');
+  }
+
+  openMobileMenu?.addEventListener('click', function() {
+    open();
+  });
+  closeMobileMenu?.addEventListener('click', function() {
+    close();
+  });
+  mobileMenu?.addEventListener('click', function() {
+    close();
+  });
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: "'Oswald', sans-serif",
+        subheading: "'Parisienne', cursive",
+        body: "'Poppins', sans-serif"
+      }
+    },
+  },
+  plugins: [],
+}
+
